7d78c6405a91dbf97d7ae320430bbcbb23424f1f
Andrew Karpenko <a.karpenko@onpoint.ru>
2026-02-17 21:07:28 +0300

    сделал авторизацию

--- backend/server.ts
+++ backend/server.ts
@@ -1,17 +1,120 @@
 import dotenv from "dotenv";
 import express from "express";
-
-console.log("start");
+import cors from "cors";
+import { createUser, findUserByEmail, updateUserRefreshToken } from "./db";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
+import { requireAuth } from "./jwt";
 
 dotenv.config();
 const app = express();
 const port = Number(process.env.PORT) || 3000;
 
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  }),
+);
+
 app.get("/", (req, res) => {
-  res.send("test");
+  res.send("ok");
 });
 
-console.log(port);
+app.get("/me", requireAuth, (req, res) => {
+  const user = (req as any).user;
+  res.json({
+    id: user.id,
+    email: user.email,
+  });
+});
+
+app.get("/auth/callback", async (req, res) => {
+  const { code } = req.query;
+
+  if (!code || typeof code !== "string") {
+    return res.status(400).send("Missing code");
+  }
+
+  const params = new URLSearchParams({
+    client_id: process.env.GOOGLE_CLIENT_ID!,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET!,
+    redirect_uri: process.env.GOOGLE_REDIRECT_URI!,
+    grant_type: "authorization_code",
+    code,
+  });
+
+  const response = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params.toString(),
+  });
+  try {
+    const tokenResponse = await response.json();
+
+    const google_refresh_token = `${tokenResponse.refresh_token}`;
+    const google_access_token = `${tokenResponse.access_token}`;
+
+    const emailResponse = await fetch(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${google_access_token}` },
+      },
+    );
+    const userInfo = await emailResponse.json();
+    const email = `${userInfo.email}`;
+    let user = findUserByEmail(email);
+
+    if (!user) {
+      user = createUser({ email, google_refresh_token });
+    } else {
+      updateUserRefreshToken(email, google_refresh_token);
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET!, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 дней в миллисекундах
+    });
+
+    res.redirect(process.env.FRONT_URL!);
+  } catch (error) {
+    console.log(error);
+    res.redirect(process.env.FRONT_URL!);
+  }
+});
+
+app.post("/auth", (req, res) => {
+  const authUrl =
+    "https://accounts.google.com/o/oauth2/v2/auth?" +
+    "client_id=" +
+    process.env.GOOGLE_CLIENT_ID +
+    "&redirect_uri=" +
+    process.env.GOOGLE_REDIRECT_URI +
+    "&response_type=code" +
+    "&scope=https://www.googleapis.com/auth/userinfo.email" +
+    "&access_type=offline" +
+    "&prompt=consent";
+
+  res.json({ authUrl });
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+    path: "/",
+  });
+  res.json({ ok: true });
+});
+
 app.listen(port, () => {
   console.log(`Listening ${port}`);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,65 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
+const API_BASE = "http://localhost:3000";
+
 function App() {
+  const [user, setUser] = useState<{ email: string } | null>(null);
+  const [isAuth, setIsAuth] = useState(false);
+
+  const handleClick = async () => {
+    const response = await fetch(`${API_BASE}/auth`, { method: "POST" });
+    const { authUrl } = await response.json();
+    window.location.href = authUrl;
+  };
+
+  const handleLogout = async () => {
+    const response = await fetch(`${API_BASE}/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
+    const { ok } = await response.json();
+    if (ok) {
+      setIsAuth(false);
+      setUser(null);
+    }
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await fetch(`${API_BASE}/me`, {
+          method: "GET",
+          credentials: "include",
+        });
+        if (response.status === 401) {
+          setIsAuth(false);
+          return;
+        }
+        setIsAuth(true);
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.log(error);
+        setIsAuth(false);
+      }
+    };
+    getUser();
+  }, []);
+
   return (
     <>
       <div>
-        <button>123</button>
+        <button onClick={handleClick}>Sign in</button>
+        {isAuth ? (
+          <div>
+            <p>is auth</p>
+            <p>{user?.email}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <p> no auth</p>
+        )}
       </div>
     </>
   );
